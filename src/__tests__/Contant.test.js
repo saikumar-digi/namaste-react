3347ef17729e519e200c8955d820f66e89e4cc8a
saikumar <saikumar@digisprint.com>
2024-03-22 16:18:12 +0530

    added test cases

--- src/__tests__/Contant.test.js
+++ src/__tests__/Contant.test.js
@@ -6,6 +6,28 @@ test("should load contact us component", () => {
   render(<Contact />);
   const heading = screen.getByRole("heading")
   //assertion
-  expect(heading).toBeInTheDocument();
+  expect(heading).toBeInTheDocument()
 
 });
+
+test("should load button insode contact us component", () => {
+    render(<Contact />);
+    // const button = screen.getByRole("button")
+    const button = screen.getByText("Submit")
+
+    //assertion
+    expect(button).toBeInTheDocument();
+  
+  });
+
+
+
+  test("should load input insode contact us component", () => {
+    render(<Contact />);
+    //querying
+    const input_box = screen.getAllByRole("textbox")
+
+    //assertion
+    expect(input_box.length).toBe(2);
+  
+  });

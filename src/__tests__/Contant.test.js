efe8b84f56e3970f30c4cd6d127a700a1953170c
saikumar <saikumar@digisprint.com>
2024-03-25 10:42:05 +0530

    added describe and it test alias

--- src/__tests__/Contant.test.js
+++ src/__tests__/Contant.test.js
@@ -1,33 +1,30 @@
 import { render, screen } from "@testing-library/react";
 import Contact from "../components/Contact";
-import "@testing-library/jest-dom"
+import "@testing-library/jest-dom";
 
-test("should load contact us component", () => {
-  render(<Contact />);
-  const heading = screen.getByRole("heading")
-  //assertion
-  expect(heading).toBeInTheDocument()
+describe("conatact us test case", () => {
+  test("should load contact us component", () => {
+    render(<Contact />);
+    const heading = screen.getByRole("heading");
+    //assertion
+    expect(heading).toBeInTheDocument();
+  });
 
-});
-
-test("should load button insode contact us component", () => {
+  it("should load button insode contact us component", () => {
     render(<Contact />);
     // const button = screen.getByRole("button")
-    const button = screen.getByText("Submit")
+    const button = screen.getByText("Submit");
 
     //assertion
     expect(button).toBeInTheDocument();
-  
   });
 
-
-
-  test("should load input insode contact us component", () => {
+  it("should load input insode contact us component", () => {
     render(<Contact />);
     //querying
-    const input_box = screen.getAllByRole("textbox")
+    const input_box = screen.getAllByRole("textbox");
 
     //assertion
     expect(input_box.length).toBe(2);
-  
   });
+});
